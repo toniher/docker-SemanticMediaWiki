da4b080c992118c88176bc9385ae9f6b70f5d7ba
Toni Hermoso Pulido <toniher@cau.cat>
2016-11-25 00:11:02 +0100

    some configuration params

--- LocalSettings.local.php
+++ LocalSettings.local.php
@@ -2,3 +2,14 @@
 
 #  Local configuration for MediaWiki
 
+ini_set( 'max_execution_time', 1000 );
+ini_set('memory_limit', '-1'); 
+
+$wgEnableUploads = true;
+
+wfLoadExtension('ParserFunctions');
+wfLoadExtension('WikiEditor');
+
+$wgArticlePath = "/wiki/$1";
+
+
