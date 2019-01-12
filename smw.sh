f0103cf4cd732ba89ee2e477b444907c83a26d9d
Toni Hermoso Pulido <toniher@cau.cat>
2019-01-12 16:40:12 +0100

    Update making it easier

--- smw.sh
+++ smw.sh
@@ -1,21 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-MEDIAWIKI_VERSION=1.27
-MEDIAWIKI_FULL_VERSION=1.27.4
-DOMAIN_NAME=mywiki
-MYSQL_ROOT_PASSWORD=mediawiki
-MYSQL_DATABASE=mediawiki
-MYSQL_USER=mediawiki
-MYSQL_PASSWORD=mediawiki
-MYSQL_PREFIX=mw_
-MW_PASSWORD=prova123
-MW_SCRIPTPATH=/w
-MW_WIKINAME=MyWiki
-MW_WIKIUSER=WikiSysop
-MW_EMAIL=i@mywiki.com
-PORT=10080
+source <(sed -E -n 's/[^#]+/export &/ p' vars.env)
 
-docker pull mariadb:10.1
+docker pull mariadb:$MARIADB_TAG
 docker run --name db -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -e MYSQL_DATABASE=$MYSQL_DATABASE -e MYSQL_USER=$MYSQL_USER -e MYSQL_PASSWORD=$MYSQL_PASSWORD -v $(pwd)/data/db:/var/lib/mysql -v $(pwd)/mariadb-custom.cnf:/etc/mysql/conf.d/custom.cnf -p 4306:3306 -d mariadb:10.1
 
 MARIADB_HOST=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' db`
