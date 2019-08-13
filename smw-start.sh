c3c5c788d442b1032d54d66e308723151ddf2fe0
Toni Hermoso Pulido <toniher@cau.cat>
2019-08-13 16:55:42 +0200

    update and fixing raspbian

--- smw-start.sh
+++ smw-start.sh
@@ -1,25 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-MEDIAWIKI_VERSION=1.27
-MEDIAWIKI_FULL_VERSION=1.27.4
-DOMAIN_NAME=mywiki
-MYSQL_ROOT_PASSWORD=mediawiki
-MYSQL_DATABASE=mediawiki
-MYSQL_USER=mediawiki
-MYSQL_PASSWORD=mediawiki
-MYSQL_PREFIX=mw_
-MW_PASSWORD=prova
-MW_SCRIPTPATH=/w
-MW_WIKINAME=MyWiki
-MW_WIKIUSER=WikiSysop
-MW_EMAIL=i@mywiki.com
-PORT=10080
+source <(sed -E -n 's/[^#]+/export &/ p' vars.env)
 
-docker run --name db -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -e MYSQL_DATABASE=$MYSQL_DATABASE -e MYSQL_USER=$MYSQL_USER -e MYSQL_PASSWORD=$MYSQL_PASSWORD -v $(pwd)/data/db:/var/lib/mysql -v $(pwd)/mariadb-custom.cnf:/etc/mysql/conf.d/custom.cnf -p 4306:3306 -d toniher/rpi-mariadb:latest
+docker run --name $MARIADB_CONTAINER -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -e MYSQL_DATABASE=$MYSQL_DATABASE -e MYSQL_USER=$MYSQL_USER -e MYSQL_PASSWORD=$MYSQL_PASSWORD -v $(pwd)/data/db:/var/lib/mysql -v $(pwd)/mariadb-custom.cnf:/etc/mysql/conf.d/custom.cnf -p $PORT_DB:3306 -d mariadb:10.1
 
-MARIADB_HOST=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' db`
+MARIADB_HOST=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' $MARIADB_CONTAINER`
 
 echo $MARIADB_HOST
 
-docker run -p $PORT:80 --name smw -d smw
-
+docker run -p $PORT:80 --name $WIKI_CONTAINER -d smw
