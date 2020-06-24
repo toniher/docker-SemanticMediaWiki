426e4f1505646b65c1ed3ef9b8cfcbc807e3b823
Toni Hermoso Pulido <toniher@cau.cat>
2020-06-24 14:11:35 +0200

    passing vars

--- smw-start.sh
+++ smw-start.sh
@@ -4,9 +4,9 @@ VARS=${1:-vars.env}
 
 source <(sed -E -n 's/[^#]+/export &/ p' $VARS)
 
-bash smw-start-db.sh
+bash smw-start-db.sh $VARS
 
 docker network connect $NETWORK $DB_CONTAINER
 
-bash smw-start-wiki.sh
+bash smw-start-wiki.sh $VARS
 
