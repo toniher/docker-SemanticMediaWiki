65215a8a470418effad4b599ea03c8adc9e8ec55
Toni Hermoso Pulido <toniher@cau.cat>
2021-04-22 18:22:43 +0200

    update

--- smw-start-wiki.sh
+++ smw-start-wiki.sh
@@ -6,24 +6,50 @@ source <(sed -E -n 's/[^#]+/export &/ p' $VARS)
 
 echo "Running wiki"
 
+# SPECIFIC MOUNTS
 LOCALSETTINGS_MOUNT=""
+LOCALSETTINGS_LOCAL_MOUNT=""
+LOCALSETTINGS_REDIS_MOUNT=""
+LOGO_MOUNT=""
+SCRATCH_MOUNT=""
+CUSTOMIZATIONS_MOUNT=""
+MSMTP_MOUNT=""
 
-if [ ! $MW_NEW ]; then 
+if [ ! $MW_NEW ]; then
 	LOCALSETTINGS_MOUNT="-v ${CONF_PATH}/LocalSettings.php:/var/www/w/LocalSettings.php"
 fi
 
-# envsubst msmtprc.template > ${CONF_PATH}/msmtprc
+if [ -f ${CONF_PATH}/LocalSettings.local.php ]; then
+	LOCALSETTINGS_LOCAL_MOUNT="-v ${CONF_PATH}/LocalSettings.local.php:/var/www/w/LocalSettings.local.php"
+fi
+
+if [ -f ${CONF_PATH}/LocalSettings.redis.php ]; then
+	LOCALSETTINGS_LOCAL_MOUNT="-v ${CONF_PATH}/LocalSettings.redis.php:/var/www/w/LocalSettings.redis.php"
+fi
+
+if [ -f ${CONF_PATH}/logo.png ]; then
+	LOGO_MOUNT="-v ${CONF_PATH}/logo.png:/var/www/w/logo.png"
+fi
+
+if [ -d ${SCRATCH} ]; then
+	SCRATCH_MOUNT="-v ${SCRATCH}:/scratch"
+fi
+
+if [ -d "${CONF_PATH}/customizations" ]; then
+	CUSTOMIZATIONS_MOUNT="-v ${CONF_PATH}/customizations:/var/www/w/customizations"
+fi
+
+if [ -f "${CONF_PATH}/msmtprc" ]; then
+	MSMTP_MOUNT="-v ${CONF_PATH}/msmtprc:/etc/msmtprc"
+fi
 
 docker run --net=$NETWORK -p $PORT:80 -v ${MW_IMAGES}:/var/www/w/images \
-${LOCALSETTINGS_MOUNT} -v ${CONF_PATH}/LocalSettings.local.php:/var/www/w/LocalSettings.local.php \
--v ${CONF_PATH}/LocalSettings.redis.php:/var/www/w/LocalSettings.redis.php -v ${CONF_PATH}/LocalSettings.cirrus.php:/var/www/w/LocalSettings.cirrus.php \
--v ${CONF_PATH}/msmtprc:/etc/msmtprc \
+${LOCALSETTINGS_MOUNT} ${LOCALSETTINGS_LOCAL_MOUNT} ${LOCALSETTINGS_REDIS_MOUNT} \
+${LOGO_MOUNT} \
+${SCRATCH_MOUNT} \
+${CUSTOMIZATIONS_MOUNT} \
+${MSMTP_MOUNT} \
 --name $WIKI_CONTAINER --network-alias=$DOMAIN_NAME -d $WIKI_IMAGE
-
-# Other potential params above
-#-v ${CONF_PATH}/logo.png:/var/www/w/logo.png \
-#-v ${SCRATCH}:/scratch \
-#-v ${CONF_PATH}/customizations:/var/www/w/customizations \
 
 echo "Running parsoid"
 
@@ -37,4 +63,4 @@ docker run --net=$NETWORK --name $PARSOID_CONTAINER -d -p 8142:8000 \
 docker exec $WIKI_CONTAINER php /var/www/w/maintenance/update.php
 docker exec $WIKI_CONTAINER php /var/www/w/maintenance/runJobs.php
 
-# bash updateEmail.sh $VARS
+#bash updateEmail.sh $VARS
--- updateEmail.sh
+++ updateEmail.sh
@@ -4,5 +4,5 @@ VARS=${1:-vars.env}
 
 source <(sed -E -n 's/[^#]+/export &/ p' $VARS)
 
-mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h${MARIADB_HOST} ${MYSQL_DATABASE} -e "update mw_user set user_email=\"${MW_EMAIL}\" where user_name=\"${MW_WIKIUSER}\"; "
+mysql -u${MYSQL_USER} -P${PORT_DB} -p${MYSQL_PASSWORD} -h${MARIADB_HOST:-0.0.0.0} ${MYSQL_DATABASE} -e "update mw_user set user_email=\"${MW_EMAIL}\" where user_name=\"${MW_WIKIUSER}\"; "
 
