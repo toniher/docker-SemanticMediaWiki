ea4dd235bf5402af4bba99662c5330a4f5d8730e
Toni Hermoso Pulido <toni.hermoso@crg.eu>
2023-07-04 16:07:55 +0200

    upgrade to allow more flexibility

--- download-extension-git.sh
+++ download-extension-git.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 EXTENSION=$1
-BRANCH=$2
-EXTDIR=$3
+TARGET=$2
+OUTDIR=$3
+TAG=${4:-0}
+SKIN=${5:-0}
+TYPEDIR=${6:-extensions}
 
-TAR_URL="https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/$EXTENSION/+archive/REL$BRANCH.tar.gz"
+OUT="REL$TARGET"
+if [ "$TAG" = "1" ]; then
+  OUT=$TARGET
+fi
+
+if [ "$SKIN" = "1" ]; then
+  TYPEDIR=skins
+fi
+
+TAR_URL="https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/$TYPEDIR/$EXTENSION/+archive/$OUT.tar.gz"
 echo $TAR_URL
 curl -s "$TAR_URL" -o "/tmp/$EXTENSION".tar.gz
 
-mkdir -p $EXTDIR
-tar -xzf /tmp/$EXTENSION.tar.gz -C $EXTDIR && \
-    rm /tmp/$EXTENSION.tar.gz
+mkdir -p $OUTDIR
+tar -xzf /tmp/$EXTENSION.tar.gz -C $OUTDIR && \
+  rm /tmp/$EXTENSION.tar.gz
+
